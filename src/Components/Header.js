c6efa165ca4ee5bb599ec57f204fce75cda301ff
kelar crisp <kelcrisp007@icloud.com>
2020-05-27 14:07:56 -0700

    basic setup and most of item movement is complete

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className={'row'}>
+            <div className={'page-header'}>👽</div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Components/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () => {
+    return (
+        <div>
+            time to start odg
+        </div>
+    );
+};
+
+export default Homepage;
